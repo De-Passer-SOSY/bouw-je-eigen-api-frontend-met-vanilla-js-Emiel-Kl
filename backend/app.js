1a2aed7e459f31ab5332a95e70e7c61fe811cdef
Emiel-Kl <emiel.kleppe@leerling.de-passer.be>
2025-05-08 11:48:33 +0200

    database fetch
    app.js fetches database

--- backend/app.js
+++ backend/app.js
@@ -6,5 +6,15 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+
+app.get('/chessChampions', async (req, res) => {
+    try{
+        const chessChampions = await db('wereldkampioenen');
+        res.status(200).json(chessChampions);
+    }catch{
+        res.status(500).json({message:'internal server error'});
+    }
+})
+
 app.listen(3333);
 console.log('Server is running on port 3333');

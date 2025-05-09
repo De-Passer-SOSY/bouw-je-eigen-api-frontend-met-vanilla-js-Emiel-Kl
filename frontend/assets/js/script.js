720006d2a478fa1ef41d3f2e7e46c1fbea01d418
Emiel-Kl <emiel.kleppe@leerling.de-passer.be>
2025-05-09 16:28:31 +0200

    split fetch and display function

--- frontend/assets/js/script.js
+++ frontend/assets/js/script.js
@@ -1,28 +1,29 @@
 "use strict";
 
-
 document.addEventListener("DOMContentLoaded", init);
 
 function init() {
     console.log("De pagina is volledig geladen");
     fetchData();
 }
-
 async function fetchData() {
     try {
         let response = await fetch("http://localhost:3333/chessChampions");
         let chessChampions = await response.json();
         console.log(chessChampions);
+        displayData(chessChampions);
 
-        let listElement = document.getElementById("champions-list");
-
-        for (let champion of chessChampions) {
-            let listItem = document.createElement("li");
-
-            listItem.textContent = `${champion.naam} - ${champion.nationaliteit} (${champion.jaar})`;
-            listElement.appendChild(listItem);
-        }
     } catch (error) {
         console.error("Fout bij het ophalen van kampioenen:", error);
+    }
+}
+function displayData(chessChampions){
+    let listElement = document.getElementById("champions-list");
+
+    for (let champion of chessChampions) {
+        let listItem = document.createElement("li");
+
+        listItem.textContent = `${champion.naam} - ${champion.nationaliteit} (${champion.jaar})`;
+        listElement.appendChild(listItem);
     }
 }

c02a2837d85ea24d76d1bdea49d5cebe815f54d2
Emiel-Kl <emiel.kleppe@leerling.de-passer.be>
2025-05-15 10:36:45 +0200

    app.post corrected, app.get on id added
    app.post corrected, app.get on id added

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+
 app.get('/chessChampions', async (req, res) => {
     try{
         const chessChampions = await db('wereldkampioenen');
@@ -13,27 +14,38 @@ app.get('/chessChampions', async (req, res) => {
     }catch{
         res.status(500).json({message:'internal server error'});
     }
-})
+});
 
+app.get("/chessChampion/:id", async (req, res) => {
+    try {
+        const row = await db("wereldkampioenen").where({ id: req.params.id }).first();
+        if (row) {
+            res.status(200).json(row);
+        } else {
+            res.status(404).json({ error: "Kampioen niet gevonden" });
+        }
+    } catch (err) {
+        res.status(500).json({ error: "Database error" });
+    }
+});
 
-
-app.post('/addChessChampions', async (req, res) => {})
-{
+app.post('/newChessChampion', async (req, res) => {
     const {naam, nationaliteit, jaar} = req.body;
 
     if (!naam || !nationaliteit || !jaar) {
         return res.status(400).json({error: "Vul alle velden in"});
     }
     try {
-        const [id] = await db("absences").insert({naam, nationaliteit, jaar});
+        const [id] = await db("wereldkampioenen").insert({naam, nationaliteit, jaar});
         res.status(201).json({
             message: "Succesvol toegevoegd",
             id: id
         })
-    } catch (error) {
-        res.status(500).json({error: "Fout bij het toevoegen van de student"});
+    }catch(error){
+        res.status(500).json({error: "Fout bij het toevoegen van de kampioen"});
     }
-}
+});
+
 
 app.listen(3333);
 console.log('Server is running on port 3333');
